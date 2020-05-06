c1dd8e46fcc39d5c533337dfddeaa33ac0b6739f
Aybetul <aybetul@hotmail.com>
2020-05-06 00:36:11 -0500

    added navigate method

--- src/test/java/com/briteErp/pages/AbstractPageBase.java
+++ src/test/java/com/briteErp/pages/AbstractPageBase.java
@@ -1,8 +1,12 @@
 package com.briteErp.pages;
 
 import com.briteErp.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class AbstractPageBase {
@@ -13,8 +17,15 @@ public abstract class AbstractPageBase {
         PageFactory.initElements(driver,this);
     }
 
+    public void navigateTo( String moduleName) {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[contains(text(),'Notes')]")));
+        String moduleXpath = "//span[contains(text(),'"+moduleName+"')]";
+        WebElement moduleElement = driver.findElement(By.xpath(moduleXpath));
+          moduleElement.click();
+    }
 
 
 
 
+git
 }
